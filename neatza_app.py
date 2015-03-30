58e354d1d437b6b4d7dd1e8f1d71e80b15614451
Alexandru Ardelean <ardeleanalex@gmail.com>
2015-03-30 09:59:49 +0000

    neatza-app: refactor main code
    We'll prefer to use fresh sources from the internet.
    The cache is running out.

--- neatza_app.py
+++ neatza_app.py
@@ -38,7 +38,6 @@ import ConfigParser
 log.getLogger("requests").setLevel(log.WARNING)
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
-CNF_DIR = os.path.join( APP_DIR, 'conf' )
 LOG_DIR = os.path.join( APP_DIR, 'log' )
 CACHE_DIR = os.path.join( APP_DIR, 'cache' )
 
@@ -89,6 +88,28 @@ def extract_an_url(fname):
 
     return (url)
 
+def _get_url_for_name( sources, group ):
+
+    if (group not in sources):
+        return
+    source_group = sources[group]
+
+    src = None
+    url = None
+    for s in source_group['sources']:
+        if (source_group.get( s + '_used' )):
+            continue
+        module = __import__('scrapers.' + s, fromlist = [ 'get_url' ])
+        url = module.get_url()
+        if (url):
+            source_group[ s + '_used' ] = True
+            return url
+
+    for s in source_group['sources']:
+        url = extract_an_url( os.path.join( CACHE_DIR, s ) )
+        if (url):
+            return url
+
 def _get_to_addrs(config, tag, default_dst_addr = None):
 
     to_addrs = []
@@ -102,17 +123,23 @@ def _get_to_addrs(config, tag, default_dst_addr = None):
 
     return to_addrs
 
-def _send_neatza( server, from_addr, tag, qotds, bash_data, img_url, to_addrs ):
+def _get_bash_text( bash_data ):
 
     bash_fresh, bash_cache = bash_data
 
-    quote, qauth = "", ""
-    if (len(qotds) > 0):
-        quote, qauth = qotds.pop()
     bash_text = ""
     if (len(bash_fresh) > 0):
         bash_id, bash_text = bash_fresh.pop()
         bash_cache.add(bash_id)
+
+    return bash_text
+
+def _send_neatza( server, from_addr, tag, qotds, bash_data, img_url, to_addrs ):
+
+    quote, qauth = "", ""
+    if (len(qotds) > 0):
+        quote, qauth = qotds.pop()
+    bash_text = _get_bash_text( bash_data )
 
     subject = u'[%s] from neatza app' % strftime("%Y-%m-%d", gmtime())
     msg_text = (u'Random Quote Of The Day for %s\n%s\n%s' % (tag.title(), quote, qauth)) + \
@@ -134,39 +161,70 @@ def _send_neatza( server, from_addr, tag, qotds, bash_data, img_url, to_addrs ):
                 msg_text  = msg_text,
                 msg_html  = msg_html)
 
+def _build_group_reverse_map(config, section):
+    rev_map = {}
+    for i in range(1,9999):
+        try:
+            id_ = 'group%d' % i
+            names = [ n.strip() for n in config.get ( section, id_ ).split(',') ]
+            for name in names:
+                rev_map[name] = id_
+        except:
+            break
+
+    return rev_map
+
+def _build_group_map(config, section):
+    map_ = {}
+
+    for i in range(1,9999):
+        try:
+            id_ = 'group%d' % i
+            names = [ n.strip() for n in config.get ( section, id_ ).split(',') ]
+            map_[id_] = { section : names }
+        except:
+            break
+
+    return map_
+
 def main():
     qotds = get_qotds()
 
+    # Read config file
     config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.readfp( open( os.path.join( APP_DIR, 'app.settings' ) ) )
 
+    # Login credentials
     email_addr = config.get( 'mail', 'address' )
     email_pass = config.get( 'mail', 'password' )
+    # Default destination email
     default_dst_addr = config.get ( 'mail', 'destination' )
 
-    files_in_cnf_dir = os.listdir(CNF_DIR)
-    random.shuffle( files_in_cnf_dir )
+    # Build reverse maps for names to group names
+    names_map = _build_group_reverse_map( config, 'names' )
+    sources   = _build_group_map( config, 'sources' )
+
+    # Get the names into a list and shuffle them
+    names = list(names_map.keys())
+    random.shuffle( names )
+
+    # Get a list of random IDs and the ones we've sent (and cached)
     bash_data = (bash.get_randoms(), bash.get_cache())
 
     server = email1.get_server(email_addr, email_pass)
 
-    for fname in files_in_cnf_dir:
-        if (not fname.endswith('.conf')):
-            continue
+    for name in names:
 
-        tag = fname[:-len('.conf')]
-
-        to_addrs = _get_to_addrs(config, tag, default_dst_addr)
+        to_addrs = _get_to_addrs(config, name, default_dst_addr)
         if (len(to_addrs) == 0):
             continue
 
-        full_fname = os.path.join( CNF_DIR, fname )
-        url = extract_an_url( full_fname )
+        url = _get_url_for_name( sources, names_map[name] )
 
         if (url is None):
-            log.warning("No image URL for '%s'", tag)
+            log.warning("No image URL for '%s'", name)
 
-        _send_neatza( server, email_addr, tag, qotds, bash_data, url, to_addrs )
+        _send_neatza( server, email_addr, name, qotds, bash_data, url, to_addrs )
 
     server.quit()
     bash.save_cache (bash_data[1])
