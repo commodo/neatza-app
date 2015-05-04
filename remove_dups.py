5dd268d09e33542e08abd1a0e028afd824e007be
Alexandru Ardelean <ardeleanalex@gmail.com>
2015-05-04 22:49:20 +0300

    remove_dups.py: change implementation to imagehash

--- remove_dups.py
+++ remove_dups.py
@@ -2,8 +2,9 @@
 
 # -*- coding: utf-8 -*-
 
-from utils import cache_object, app_prep, load_image_from_url, dhash
+from utils import cache_object, app_prep, load_image_from_url
 import traceback
+import imagehash
 
 import logging as log
 
@@ -11,14 +12,33 @@ def main():
 
     log.info( "Caching all images in memory " )
     c1 = cache_object( 'group2.moderate' )
-    images = [ (el, load_image_from_url(el) ) for el in c1 ]
-    images = [ (el, dhash(img)) for el, img in images if img ]
-    log.info( "Caching done" )
-    for f1, h1 in images:
-        for f2, h2 in images:
-            if (h1 == h2):
-                print str((f1, f2)), "are similar"
 
+    #hashfunc = imagehash.average_hash
+    hashfunc = imagehash.phash
+    #hashfunc = imagehash.dhash
+
+    images = {}
+    for url in c1:
+        img = load_image_from_url(url)
+        if (img):
+            hash = hashfunc( img )
+            images[hash] = images.get(hash, []) + [url]
+        else:
+            log.warning( url + " not loaded" )
+    
+    log.info( "Caching done" )
+
+    for k, img_list in images.iteritems():
+        if len(img_list) > 1:
+            saved_one = False
+            log.info("--------------Duplicates------------------")
+            for img in img_list:
+                if (saved_one):
+                    c1.remove(img)
+                log.info(img)
+                saved_one = True
+
+    c1.save()
 
 if __name__ == "__main__":
     app_prep( 'remove_dups.log' )
